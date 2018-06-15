39cf35f056af33407da3cfccd9c3e6743966c8cf
YAMAHAH <75268967@qq.com>
2018-06-15 19:18:59 +0800

    add

---
+++ sale-order-sample-service/src/main/java/com/universesoft/saleordersampleservice/SaleOrderSampleServiceApplication.java
@@ -0,0 +1,12 @@
+package com.universesoft.saleordersampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleOrderSampleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleOrderSampleServiceApplication.class, args);
+	}
+}
