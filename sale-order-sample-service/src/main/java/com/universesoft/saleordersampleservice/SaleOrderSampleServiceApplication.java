35f81002fc2b530685aa15ffd0b8c02c2d725fe4
YAMAHAH <75268967@qq.com>
2018-06-15 19:34:42 +0800

    sale-order-sample-service

--- sale-order-sample-service/src/main/java/com/universesoft/saleordersampleservice/SaleOrderSampleServiceApplication.java
+++ sale-order-sample-service/src/main/java/com/universesoft/saleordersampleservice/SaleOrderSampleServiceApplication.java
@@ -1,5 +1,7 @@
 package com.universesoft.saleordersampleservice;
 
+import com.universesoft.domainmodule.domain.event.partner.CreatedPartnerEvent;
+import com.universesoft.domainmodule.domain.event.product.CreatedProductEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,7 @@ public class SaleOrderSampleServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaleOrderSampleServiceApplication.class, args);
+		CreatedProductEvent createdProductEvent = new CreatedProductEvent();
+		CreatedPartnerEvent createdPartnerEvent = new CreatedPartnerEvent();
 	}
 }
